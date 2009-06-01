4bbbb9a49f11caf507ba3aebbb92a15bb4fda2dd
Claudio Perez Gamayo <crossblaim@gmail.com>
2009-06-01 21:23:04 +0200

    Start to translate the site to english and spanish

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,11 @@ class ApplicationController < ActionController::Base
   include SubstructApplicationController  
   before_filter :set_substruct_view_defaults
   before_filter :get_nav_tags
-  before_filter :find_customer
+  before_filter :find_customer          
+  before_filter :set_locale
+
+  def set_locale   
+    session[:locale] = params[:locale] if params[:locale]
+    I18n.locale = session[:locale] || I18n.default_locale
+  end
 end
