d81703966a00c6fb02cf2ef11ee814b01d6826f6
Claudio Perez Gamayo <crossblaim@gmail.com>
2009-06-01 18:33:31 +0200

    Fix routes of substruct engine

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 ActionController::Routing::Routes.draw do |map|
-  map.from_plugin :substruct
-
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
