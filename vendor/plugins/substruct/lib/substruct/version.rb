d1af88fcfd1d18bf489e428ac1295a53c9f0e02d
Michael Pirogov <vbnet.ru@gmail.com>
2009-08-17 14:04:11 +0400

    Version bump

--- vendor/plugins/substruct/lib/substruct/version.rb
+++ vendor/plugins/substruct/lib/substruct/version.rb
@@ -3,7 +3,7 @@ module Substruct
     MAJOR  = 1
     MINOR  = 0
     TINY   = 'a6'
-    MOD    = 'Mod: UU-0709'
+    MOD    = 'Mod: UU-0809'
     STRING = [MAJOR, MINOR, TINY, ":", MOD].join('.').freeze
     #TITLE  = "Noh-Varr".freeze
   end
